7896d61123ed084e386a7c391e7240df7052c9f3
doions <43123876+doions@users.noreply.github.com>
2019-09-10 18:26:49 +0530

    अपनापन पहला चेंज
    फ़र्स्ट commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `अपनापन`,
+    description: `अपनापन एक भारतीय संस्था है। यह इसका सैम्पल पेज है `,
+    author: `@अनुराग जैन`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
