2b93e7231ed237118e66678290cbaec6e51e1d73
doions <43123876+doions@users.noreply.github.com>
2019-09-10 18:34:17 +0530

    और बदलाव
    सैम्पल

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,13 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>जय जिनेंद्र</h1>
+    <p>स्वागत है आप सभी का। यह साइट अभी निर्माणाधीन है।</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">सैम्पल पृष्ट</Link>
   </Layout>
 )
 
