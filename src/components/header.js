d4b7392a081ee0f60adffaf5fa8486005bdae9c5
doions <43123876+doions@users.noreply.github.com>
2019-09-11 07:27:46 +0530

    Header Color Change
    Header colour Change

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#7d2a32`,
       marginBottom: `1.45rem`,
     }}
   >
